bcd71285fa5f0b244541a54d7aba5666d434326f
Jacob Zweifel <jzweifel@tribalscale.com>
2019-03-06 11:35:15 -0500

    initialize dotnet projects

---
+++ tests/KafkaStreams.Test/KafkaStreams.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Netcorefluent.KafkaStreams.Test
+{
+    public class KafkaStreamsTest
+    {
+        [Fact]
+        public void AssertTrue_IsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
